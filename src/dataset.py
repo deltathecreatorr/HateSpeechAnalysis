bee51677cad82274db18524bef19bae413884d8c
deltathecreatorr <julio_anandaraaj@outlook.com>
2026-03-27 23:50:58 +0000

    Add initial project files including .env example, requirements, and dataset functions

---
+++ src/dataset.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from twikit import Client
+import asyncio
+import os
+
+def getRedditDataframe() -> pd.DataFrame:
+    """
+    Returns the CSV Reddit file data as a Pandas Dataframe.
+
+    Arguments:
+        None
+    Returns:
+        reddit_dataset: A pandas Dataframe containing the Reddit dataset with the following columns: "author_name", "post_id", "comment_id", "self_text", "created_time", "user_total_karma", "user_account_created_time" the dataframe can be accessed by calling reddit_dataset['column_name'] where column_name is one of the columns mentioned above.
+    """
+    df = pd.read_csv('dataset/reddit_opinion_PSE_ISR.csv', chunksize = 1000, usecols = ["author_name", "post_id", "comment_id", "self_text", "created_time", "user_total_karma", "user_account_created_time"])
+    return df
+
+def getTwitterDataframe(filepath: str, search_query: str) -> None:
+    """
+    Scrape Twitter Data using TwiKit library. 
+    The tweets will be stored into a CSV to create my own dataset,
+    but adding twitter credentials to a .env file at the
+    root directory will also allow you to run this script.
+    
+    Arguments:
+        filepath: A string representing the path where the twitter_dataset.csv will be stored.
+    Returns:
+        None, but the function will create a CSV file named twitter_dataset.csv in the filepath from the Arguments.
+    """
+    
+    client = Client("en-US")
+
+    async def main():
+        await client.login(
+            auth_info_1 = os.getenv("USERNAME"),
+            auth_info_2 = os.getenv("PASSWORD"),
+            password = os.getenv("EMAIL"),
+            cookies_file = "cookies.json"
+        )
+        client.save_cookies("cookies.json")
+
+        tweets = await client.search_tweet(search_query, "Latest") 
+    
+    asyncio.run(main())
+
+
+
+
+
