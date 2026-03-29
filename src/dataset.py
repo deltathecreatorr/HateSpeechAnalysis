05d56dcb5cdfed2d072c0fc52865b23213dfddde
deltathecreatorr <julio_15@hotmail.co.uk>
2026-03-29 13:48:11 +0100

    fix: correct CSV reading and enhance Twitter login error handling

--- src/dataset.py
+++ src/dataset.py
@@ -12,7 +12,7 @@ def getRedditDataframe() -> pd.DataFrame:
     Returns:
         reddit_dataset: A pandas Dataframe containing the Reddit dataset with the following columns: "author_name", "post_id", "comment_id", "self_text", "created_time", "user_total_karma", "user_account_created_time" the dataframe can be accessed by calling reddit_dataset['column_name'] where column_name is one of the columns mentioned above.
     """
-    df = pd.read_csv('dataset/reddit_opinion_PSE_ISR.csv', chunksize = 1000, usecols = ["author_name", "post_id", "comment_id", "self_text", "created_time", "user_total_karma", "user_account_created_time"])
+    df = pd.read_csv('dataset/reddit_opinion_PSE_ISR.csv', usecols = ["author_name", "post_id", "comment_id", "self_text", "created_time", "user_total_karma", "user_account_created_time"])
     return df
 
 def getTwitterDataframe(filepath: str, search_query: str) -> None:
@@ -31,19 +31,27 @@ def getTwitterDataframe(filepath: str, search_query: str) -> None:
     client = Client("en-US")
 
     async def main():
-        await client.login(
-            auth_info_1 = os.getenv("USERNAME"),
-            auth_info_2 = os.getenv("PASSWORD"),
-            password = os.getenv("EMAIL"),
-            cookies_file = "cookies.json"
-        )
-        client.save_cookies("cookies.json")
+        try:
+            client.load_cookies("cookies.json")
+        except FileNotFoundError or Exception as e:
+            await client.login(
+                auth_info_1 = os.getenv("USERNAME"),
+                auth_info_2 = os.getenv("EMAIL"),
+                password = os.getenv("PASSWORD"),
+                cookies_file = "cookies.json"
+            )
+            client.save_cookies("cookies.json")
 
         tweets = await client.search_tweet(search_query, "Latest") 
+
+        for tweet in tweets:
+            print(tweet)
     
     asyncio.run(main())
 
-
+if __name__ == "__main__":
+    getRedditDataframe()
+    getTwitterDataframe("dataset/twitter_dataset.csv", "hate speech")
 
 
 
